8bb45faa8c1db2d2cfe0cd4e140749f23b07c1ad
Prodip Sengupta <prodipsengupta27@gmail.com>
2025-06-24 15:34:30 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -8,21 +8,15 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'images.unsplash.com',
+      pathname: '/**',
+    },
+  ],
+},
+
 };
 
 export default nextConfig;
